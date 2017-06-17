f8bee71b26ae9f0ed04ac1f898d96f37d111b480
Ryan Dunnewold <rmdunnewold@gmail.com>
2017-06-17 14:02:44 +0200

    Fix date

--- content.js
+++ content.js
@@ -1,10 +1,9 @@
 var transactions = document.getElementsByClassName('transaction');
 for (var i = 0; i < transactions.length; i++) {
-  var date = transactions[i].getElementsByClassName('transaction-section-title')[0].innerText
   var store = transactions[i].getElementsByClassName('transaction-title')[0].innerHTML
   var price = transactions[i].getElementsByClassName('transaction-amount')[0].innerHTML.replace('$', '')
   var link = document.createElement("a");
-  link.href = "https://www.spendlyapp.com/transactions/new?price=" + price + "&store=" + store + "&date=" + date;
+  link.href = "https://www.spendlyapp.com/transactions/new?price=" + price + "&store=" + store;
   link.target = "_blank";
   link.innerText = 'Add to Spendly';
   link.className = 'chrome-spendly-button'
