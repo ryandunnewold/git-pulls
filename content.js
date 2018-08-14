d19f013fee0f498562e62377942d9a22bec762a9
Ryan Dunnewold <rmdunnewold@gmail.com>
2018-08-14 16:26:37 -0500

    Open prod PR on click

--- content.js
+++ content.js
@@ -1,11 +1,5 @@
-var transactions = document.getElementsByClassName('transaction');
-for (var i = 0; i < transactions.length; i++) {
-  var store = transactions[i].getElementsByClassName('transaction-title')[0].innerHTML
-  var price = transactions[i].getElementsByClassName('transaction-amount')[0].innerHTML.replace('$', '')
-  var link = document.createElement("a");
-  link.href = "https://www.spendlyapp.com/transactions/new?price=" + price + "&store=" + store;
-  link.target = "_blank";
-  link.innerText = 'Add to Spendly';
-  link.className = 'chrome-spendly-button'
-  transactions[i].insertBefore(link, transactions[i].lastChild);
-}
+var baseUrl = 'https://github.com'
+var owner = document.querySelector('.author a.url').innerText
+var project = document.querySelector('[data-pjax="#js-repo-pjax-container"]').innerText
+
+window.location.href = `${baseUrl}/${owner}/${project}/compare/stable...master?expand=1&title=PROD: `
